fc704f96bb650765313e748ee977164327c2cd51
Ab-Obeng <abobengsintim@gmail.com>
2023-07-24 01:34:38 +0000

    hoping

--- purity/lib/pages/login_page.dart
+++ purity/lib/pages/login_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  final Function()? onTap;
+
+  const LoginPage({Key? key, this.onTap}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -18,146 +18,71 @@ class _LoginPageState extends State<LoginPage> {
     });
   }
 
-  Future<void> _signInWithGoogle() async {
-    try {
-      final GoogleSignIn googleSignIn = GoogleSignIn();
-      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-
-      if (googleUser == null) {
-        // User cancelled the Google Sign-In process.
-        return;
-      }
-
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      final UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
-      final User? user = authResult.user;
-
-      if (user != null) {
-        // Successfully signed in with Google. You can access the user information here.
-        print('User: ${user.displayName}, Email: ${user.email}');
-        Navigator.pushReplacementNamed(context, '/home'); // Navigate to the home page after successful login
-      } else {
-        // Sign-in process failed.
-        print('Sign-in process failed.');
-      }
-    } catch (e) {
-      print('Error during Google Sign-In: $e');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            // Background Image
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('lib/assets/icons/waterpic.jpeg'), // Replace 'background.jpg' with your actual background image path
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Opacity(
-              opacity: 0.9,
-              child: SingleChildScrollView(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 150.0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 150.0),
-                      child: Column(
-                        children: [
-                          WaterIconImage(),
-                          SizedBox(height: 15),
-                          Text(
-                            'Purity',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+                    WaterIconImage(),
+                    SizedBox(height: 16),
+                    Text(
+                      'Purity',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ),
-                    const SizedBox(height: 140),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15.0),
-                          border: Border.all(color: Colors.blueAccent, width: 2.0),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 8.0),
-                          child: TextField(
-                            style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)), // Set the text color for the input field
-                            decoration: InputDecoration(
-                              labelText: 'Enter your mail',
-                              labelStyle: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                
-                              ),
-                              border: InputBorder.none,
-                            ),
-                            autofillHints: [AutofillHints.email],
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 40),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          
-                          borderRadius: BorderRadius.circular(15.0),
-                          border: Border.all(color: Colors.blueAccent, width: 2.0),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: TextField(
-                            style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)), // Set the text color for the input field
-                            obscureText: !_isPasswordVisible,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              labelStyle: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              
-                              ),
-                              border: InputBorder.none,
-                              suffixIcon: IconButton(
-                                icon: Icon(
-                                  _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                                ),
-                                onPressed: _togglePasswordVisibility,
-                              ),
-                            ),
-                            autofillHints: const [AutofillHints.password],
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 40),
-                    ElevatedButton(
-                      onPressed: _signInWithGoogle,
-                      child: const Text('Sign In'),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 140),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your mail',
+                  ),
+                  autofillHints: [AutofillHints.email],
+                ),
+              ),
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: TextField(
+                  obscureText: !_isPasswordVisible,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: _togglePasswordVisibility,
+                    ),
+                  ),
+                  autofillHints: const [AutofillHints.password],
+                ),
+              ),
+
+              // log in button
+              GestureDetector(
+                onTap: widget.onTap,
+                child: const SizedBox(height: 16),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/home');
+                },
+                child: const Text('Log in'),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -174,5 +99,3 @@ class WaterIconImage extends StatelessWidget {
     );
   }
 }
-
-
